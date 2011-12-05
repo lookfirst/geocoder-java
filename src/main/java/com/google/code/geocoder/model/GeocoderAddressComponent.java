d18890c4a43e184b6348460c06268cc857a70e36
Jon Stevens <latchkey@gmail.com>
2011-12-04 18:52:29 -0800

    updated jars, add lombok, switch to jackson
    I've switched from Gson to Jackson. Cut out all of the
    .equals/.hashcode/getter/setter code and used Lombok instead. Upgraded
    to commons-lang3. All tests pass.

--- src/main/java/com/google/code/geocoder/model/GeocoderAddressComponent.java
+++ src/main/java/com/google/code/geocoder/model/GeocoderAddressComponent.java
@@ -3,70 +3,20 @@ package com.google.code.geocoder.model;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author <a href="mailto:panchmp@gmail.com">Michael Panchenko</a>
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GeocoderAddressComponent implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private String longName;
-    private String shortName;
-    private List<String> types;
-
-    public String getLongName() {
-        return longName;
-    }
-
-    public void setLongName(String longName) {
-        this.longName = longName;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public void setShortName(String shortName) {
-        this.shortName = shortName;
-    }
-
-    public List<String> getTypes() {
-        return types;
-    }
-
-    public void setTypes(List<String> types) {
-        this.types = types;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GeocoderAddressComponent that = (GeocoderAddressComponent) o;
-
-        if (longName != null ? !longName.equals(that.longName) : that.longName != null) return false;
-        if (shortName != null ? !shortName.equals(that.shortName) : that.shortName != null) return false;
-        if (types != null ? !types.equals(that.types) : that.types != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = longName != null ? longName.hashCode() : 0;
-        result = 31 * result + (shortName != null ? shortName.hashCode() : 0);
-        result = 31 * result + (types != null ? types.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("GeocoderAddressComponent");
-        sb.append("{longName='").append(longName).append('\'');
-        sb.append(", shortName='").append(shortName).append('\'');
-        sb.append(", types=").append(types);
-        sb.append('}');
-        return sb.toString();
-    }
+	private String longName;
+	private String shortName;
+	private List<String> types;
 }
--- src/main/java/com/google/code/geocoder/model/LatLng.java
+++ src/main/java/com/google/code/geocoder/model/LatLng.java
@@ -3,87 +3,40 @@ package com.google.code.geocoder.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author <a href="mailto:panchmp@gmail.com">Michael Panchenko</a>
  * @link http://code.google.com/intl/uk/apis/maps/documentation/javascript/reference.html#LatLng
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LatLng implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public static final int DEFAULT_PRECISION = 6;
+	private static final long serialVersionUID = 1L;
+	public static final int DEFAULT_PRECISION = 6;
 
-    private BigDecimal lat, lng;
+	private BigDecimal lat, lng;
 
-    public LatLng() {
-    }
+	/** helper constructor */
+	public LatLng(String lat, String lng) {
+		this(new BigDecimal(lat), new BigDecimal(lng));
+	}
 
-    public LatLng(final BigDecimal lat, final BigDecimal lng) {
-        this.lat = lat;
-        this.lng = lng;
-    }
+	/**
+	 * @return Returns a string of the form "lat,lng" for this LatLng. We round the lat/lng values to 6 decimal places by default.
+	 */
+	public String toUrlValue() {
+		return toUrlValue(DEFAULT_PRECISION);
+	}
 
-    public LatLng(final String lat, final String lng) {
-        this.lat = new BigDecimal(lat);
-        this.lng = new BigDecimal(lng);
-    }
-
-    public BigDecimal getLat() {
-        return lat;
-    }
-
-    public void setLat(BigDecimal lat) {
-        this.lat = lat;
-    }
-
-    public BigDecimal getLng() {
-        return lng;
-    }
-
-    public void setLng(BigDecimal lng) {
-        this.lng = lng;
-    }
-
-    /**
-     * @return Returns a string of the form "lat,lng" for this LatLng. We round the lat/lng values to 6 decimal places by default.
-     */
-    public String toUrlValue() {
-        return toUrlValue(DEFAULT_PRECISION);
-    }
-
-    /**
-     * @param precision We round the lat/lng values
-     * @return Returns a string of the form "lat,lng" for this LatLng.
-     */
-    public String toUrlValue(int precision) {
-        return lat.setScale(precision, BigDecimal.ROUND_HALF_EVEN).toString() + "," + lng.setScale(precision, BigDecimal.ROUND_HALF_EVEN).toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        LatLng latLng = (LatLng) o;
-
-        if (lat != null ? !lat.equals(latLng.lat) : latLng.lat != null) return false;
-        if (lng != null ? !lng.equals(latLng.lng) : latLng.lng != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = lat != null ? lat.hashCode() : 0;
-        result = 31 * result + (lng != null ? lng.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("LatLng");
-        sb.append("{lat=").append(lat);
-        sb.append(", lng=").append(lng);
-        sb.append('}');
-        return sb.toString();
-    }
+	/**
+	 * @param precision We round the lat/lng values
+	 * @return Returns a string of the form "lat,lng" for this LatLng.
+	 */
+	public String toUrlValue(int precision) {
+		return lat.setScale(precision, BigDecimal.ROUND_HALF_EVEN).toString() + "," + lng.setScale(precision, BigDecimal.ROUND_HALF_EVEN).toString();
+	}
 }
